c548550010e9995208891604250ad7c7d3185e57
Aadil Sengupta <aadilsengupta27@gmail.com>
2023-01-08 22:42:16 +0530

    d

--- read/settings.py
+++ read/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 #STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
