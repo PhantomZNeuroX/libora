fd7e88865ff1d543e7da07df0a89f9ff13c72920
Aadil Sengupta <aadilsengupta27@gmail.com>
2023-01-08 18:29:05 +0530

    created folder

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@ echo " BUILD START"
 
 python3.9  -m pip install -r requirements.txt
 python3.9 manage.py collectstatic  --noinput --clear
-
+mkdir static
 echo " BUILD END"
 ls
 echo "test"
+cd read
+ls
