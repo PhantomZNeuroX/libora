6a7dbf630e27b576386dc3a71c1638fb3c14366b
Aadil Sengupta <aadilsengupta27@gmail.com>
2023-01-11 00:12:45 +0530

    redid Register and Login

--- main/urls.py
+++ main/urls.py
@@ -29,5 +29,4 @@ urlpatterns = [
     path('class/student/<id>', views.StudentView),
     path('request/goal', views.read_goal),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('password_reset/done/', auth_views.PasswordResetView.as_view(template_name='password-reset.html'), name='password_reset_done'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
